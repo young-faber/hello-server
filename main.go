4e558167fbd6511d7a05bcfac518b50af862c157
young-faber <yng.fbr@gmail.com>
2026-07-27 21:40:23 +0300

    http stuff

--- main.go
+++ main.go
@@ -2,8 +2,19 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	// "path"
+)
 
 func main() {
-	fmt.Println("Hello, Wrld!")
+	fmt.Println("Hello, Web!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, Web!")
+	})
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("This is the about page. Method: %v. Path: %v\n", r.Method, r.URL.Path)
+	})
+	http.ListenAndServe(":8080", nil)
 }
