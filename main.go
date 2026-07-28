2a00160f0a809bc3d5c1aca838327743b75ed364
young-faber <yng.fbr@gmail.com>
2026-07-28 16:17:21 +0300

    REST API methods logic.

--- main.go
+++ main.go
@@ -12,48 +12,55 @@ import (
 	"io"
 )
 
+type User struct {
+	Name string
+	Age  int
+}
+
+var users []User
+
 func main() {
 	fmt.Println("Hello, Web!")
 
-	type User struct {
-		Name string
-		Age  int
-	}
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { //Function which prints message to client.
 		fmt.Fprintf(w, "Hello, Web!")
 	})
-	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) { //Function which print message to client.
 		name := r.URL.Query().Get("name")
 		fmt.Printf("This is the about page. Method: %v. Path: %v\n Username: %v\n", r.Method, r.URL.Path, name)
 		fmt.Fprintf(w, "This is the about page. Method: %v. Path: %v!\nYo! Wassup, %v ;)\n", r.Method, r.URL.Path, name)
 	})
+
 	http.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
 		var user User
-		var users []User
 
-		if r.Method == "GET" {
-			fmt.Fprint(w, r.Method)
-		} else {
-			fmt.Fprint(w, r.Method)
+		if r.Method == http.MethodPost {
+			body, err := io.ReadAll(r.Body)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			err = json.Unmarshal(body, &user)
+
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			users = append(users, user)
+
+			fmt.Fprintf(w, "Added: %v.\n", user.Name)
+		}
+		if r.Method == http.MethodGet {
+			data, err := json.Marshal(user)
+			if err != nil {
+				fmt.Println("U have some problem, guys.")
+			}
+			w.Write(data)
 		}
 
-		body, err := io.ReadAll(r.Body)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		err = json.Unmarshal(body, &user)
-
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		//append(users, user)
-
-		fmt.Println(user, user.Age, user.Name)
 	})
 
 	http.ListenAndServe(":8000", nil)
